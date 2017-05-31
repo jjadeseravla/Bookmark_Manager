a5d109b0f8119eeffa66112c52f642ce4cec8f1a
jade <jadealvares@hotmail.co.uk>
2017-05-31 17:27:31 +0100

    new tag model

---
+++ app/models/tags.rb
@@ -0,0 +1,15 @@
+
+require 'data_mapper'
+require 'dm-postgres-adapter'
+
+DataMapper::Logger.new($stdout, :debug)
+
+class Tag
+  include DataMapper::Resource
+  property :id, Serial
+  property :tag, String
+end
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.finalize
+DataMapper.auto_upgrade!
--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -10,4 +10,17 @@ feature 'submitting links' do
       expect(page).to have_content('reddit')
     end
   end
+
+  scenario 'user adds a tag' do
+    visit('/')
+    click_button 'Add new link'
+    fill_in('title', with: 'reddit')
+    fill_in('url', with: 'http://www.reddit.com')
+    fill_in('tags', with: 'news')
+    click_button 'Submit new link'
+
+    within 'ul#links' do
+      expect(page).to have_content('news')
+    end
+  end
 end
