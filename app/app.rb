7a86e2325a631325390c3f2b8eab5d3512550d97
jade <jadealvares@hotmail.co.uk>
2017-06-02 15:33:00 +0100

    now it works as had a typo b was not B in BCrypt and was redirecting in a loop

--- app/app.rb
+++ app/app.rb
@@ -7,12 +7,6 @@ class BookmarkManager < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
 
-  helpers do
-    def current_user
-      @current_user ||= User.get(session[:user_id])
-    end
-  end
-
   get '/' do
     @links = Link.all
     erb :index
@@ -46,13 +40,19 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users' do
-
+    redirect to ('/')
   end
 
   post '/users' do
-    User.create(email: params[:email], password: params[:password])
+    user = User.create(email: params[:email], password: params[:password])
     session[:user_id] = user.id
     redirect to ('/')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User
   property :password_digest, Text
 
   def password=(password)
-    self.password_digest = Bcrypt::Password.create(password)
+    self.password_digest = BCrypt::Password.create(password)
   end
 end
