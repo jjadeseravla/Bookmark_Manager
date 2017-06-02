b840eceec1fe4259a2f8e218a27b5e2513624bc2
jade <jadealvares@hotmail.co.uk>
2017-06-02 12:25:20 +0100

    extra things

--- app/app.rb
+++ app/app.rb
@@ -7,6 +7,12 @@ class BookmarkManager < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
 
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
   get '/' do
     @links = Link.all
     erb :index
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User
 
   property :id, Serial
   property :email, String
-  property :password_digest, text
+  property :password_digest, Text
 
   def password=(password)
     self.password_digest = Bcrypt::Password.create(password)
